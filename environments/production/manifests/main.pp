e3c645f6485b4a0a94690f74c52be304f6fc595e
Chris Chapman <Chris Chapman>
2015-04-20 15:38:09 +0000

    Added file node for testing (assumes that npp.6.7.7.Installer is present in /etc/puppet/environments/production/modules/thor/files/)

--- environments/production/manifests/main.pp
+++ environments/production/manifests/main.pp
@@ -1,3 +1,13 @@
 node default {
-  notice('Hello world')
+  notice('version - 0.0.9')
+
+  file { ['c:\installers']:
+    ensure => 'directory',
+  }
+  file { 'npp.6.7.7.Installer.exe':
+    source => 'puppet:///modules/thor/npp.6.7.7.Installer.exe',
+    path => 'c:\installers\npp.6.7.7.Installer.exe',
+    ensure => present,
+    source_permissions => ignore,
+  }
 }
