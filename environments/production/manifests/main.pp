68895580c2bdfd871bf78b2c6f6c211258aa0725
Chris Chapman <Chris Chapman>
2015-04-20 15:51:38 +0000

    Added logic to install Notepad++ silently

--- environments/production/manifests/main.pp
+++ environments/production/manifests/main.pp
@@ -1,13 +1,19 @@
 node default {
-  notice('version - 0.0.9')
+  notice('version - 0.0.11')
 
   file { ['c:\installers']:
     ensure => 'directory',
   }
   file { 'npp.6.7.7.Installer.exe':
     source => 'puppet:///modules/thor/npp.6.7.7.Installer.exe',
-    path => 'c:\installers\npp.6.7.7.Installer.exe',
+    path => 'c:\installers\npp-Installer.exe',
     ensure => present,
     source_permissions => ignore,
+    mode => 'u+rwx',
+  }
+  package { 'Notepad++':
+    ensure => '6.7.7',
+    source => 'c:\installers\npp-Installer.exe',
+    install_options => [ '/S' ],
   }
 }
