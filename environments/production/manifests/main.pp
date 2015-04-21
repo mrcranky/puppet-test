fdbca2152a6044eac4cc1452ab5d3cde521af6c0
Chris Chapman <Chris Chapman>
2015-04-21 09:40:16 +0000

    Tweaked cloud_file code in line with git changes on main repo (:undef insteaed of undef) - still doesn't work but looks like now it's because aws-sdk is missing

--- environments/production/manifests/main.pp
+++ environments/production/manifests/main.pp
@@ -1,26 +1,19 @@
 node default {
-  notice('version - 0.0.11')
+  notice('version - 0.0.13')
 
   file { ['c:\installers']:
     ensure => 'directory',
-  }
-  file { 'npp.6.7.7.Installer.exe':
-    source => 'puppet:///modules/thor/npp.6.7.7.Installer.exe',
-    path => 'c:\installers\npp-Installer.exe',
+  } ->
+  cloud_file { 'c:\installers\npp-Installer.exe':
     ensure => present,
-    source_permissions => ignore,
-    mode => 'u+rwx',
-  }
+    source => 'blackcompany-puppet/installers/npp.6.7.7.Installer.exe',
+    access_key_id     => 'AKIAJQGZHGFW2O7XVM7A',
+    secret_access_key => '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',
+  } ->
   package { 'Notepad++':
     ensure => '6.7.7',
     source => 'c:\installers\npp-Installer.exe',
     install_options => [ '/S' ],
-  }
-  include 'stdlib'
-  include 's3file::curl'
-  s3file { 'c:\installers\/path/to/destination/file':
-    source => 'MyBucket/the/file',
-    ensure => 'latest',
   }
 
 }
